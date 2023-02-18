74656d6d4c9f4c24afc6de1b666bb29f7e570812
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-02-18 01:41:56 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -  prints all single digit numbers of base 10 starting 
+ * main -  prints all single digit numbers of base 10 starting
  * from 0 using putchar function
  *
  * Return: Always 0
