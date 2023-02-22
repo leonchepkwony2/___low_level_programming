41fc93da39837324a2dda4b33e8d815781d27e9e
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-02-22 01:25:41 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: number to be checked
+ * Return: 1 if positive, 0 if 0, and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
