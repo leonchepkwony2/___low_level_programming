97ced76a3dfc2bd34e5bd5f20bdf9268019277c1
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-02-27 06:11:08 -0800

    completed task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = _strlen(*s);
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
