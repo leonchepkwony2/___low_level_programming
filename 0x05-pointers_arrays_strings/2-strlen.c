aa34223d8dbaafb1949c96622e47cf32a7b177c8
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-02-27 05:48:44 -0800

    This report, by its very length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,16 +3,18 @@
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- * Return: lenght of the string
+ *
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
 	int i;
 
 	i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	return (i)
+
 }
