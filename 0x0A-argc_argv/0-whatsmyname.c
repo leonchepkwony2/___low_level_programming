08cc7dd5c5005f06a33e12e6ef9cd11a35a902f9
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-03-10 11:18:15 -0800

    added task 0 : whatismyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array vector of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+}
