34301d7d78004db9fbb1e26a24b10125ed17e0dd
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-03-30 06:14:55 -0700

    fixed bugs

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,23 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 0x0a ; string to be printed
-    format db "%s", 0x0a, 0       ; format string for printf
-
-section .text
-    global main
-
+          global    main
+          extern    printf
 main:
-    ; set up stack frame
-    push rbp
-    mov rbp, rsp
-
-    ; call printf to print the string
-    mov rdi, format ; first argument: format string
-    mov rsi, hello  ; second argument: string to be printed
-    xor eax, eax    ; set RAX to 0 to indicate no floating point arguments
-    call printf     ; call printf function
-
-    ; clean up and exit
-    mov rsp, rbp
-    pop rbp
-    xor eax, eax    ; return 0 to indicate success
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
