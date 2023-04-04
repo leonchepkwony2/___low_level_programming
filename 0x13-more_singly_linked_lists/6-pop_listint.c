bb92760ba10e41b37bfe6033a360d778f20847e1
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-04-04 06:28:16 -0700

    fixed a bug on task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,15 +9,16 @@
  */
 int pop_listint(listint_t **head)
 {
+	listint_t *temp;
+	int num;
+
 	if (!head || !*head)
 		return (0);
 
-	int data = (*head)->n;
-	listint_t *tmp = *head;
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
 
-	*head = (*head)->next;
-
-	free(tmp);
-
-	return (data);
+	return (num);
 }
