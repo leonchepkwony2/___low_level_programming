7ba412573a941066da2644b247f96a3885b9ef79
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-04-06 06:27:18 -0700

    completed task 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,9 @@
 #ifndef BITS_H
 #define BITS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
