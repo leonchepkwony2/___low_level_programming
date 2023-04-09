21c1340cb01bcfd5637a57101a3226aa8b48265a
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-04-09 02:34:26 -0700

    modified 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,9 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	unsigned long int mask = 1UL << index;
-
-	*n |= mask;
+	*n |= (1UL << index);
 
 	return (1);
 }
