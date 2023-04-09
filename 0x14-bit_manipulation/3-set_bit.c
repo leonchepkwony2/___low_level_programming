dadbd71cdfc6f090b99f57bed381927d06d470a3
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-04-09 02:26:43 -0700

    fixed a bug in 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	unsigned long int mask = 1UL << index;
 
