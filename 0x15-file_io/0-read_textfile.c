1dd259ed00dc8990f88783a96b7ec035762591b8
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-04-11 04:22:37 -0700

    added task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,34 +9,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd, n, bytes_written;
-	char *buffer;
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
 
-	buffer = malloc(sizeof(char) * letters);
-
-	if (buffer == NULL)
+	if (buf == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-	n = read(fd, buffer, letters);
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != n)
+	bytes_read = read(fd, buf, letters);
+
+	if (bytes_read == -1)
 	{
-		free(buffer);
+		free(buf);
 		close(fd);
 		return (0);
 	}
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
 
-	free(buffer);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	free(buf);
 	close(fd);
 
-	return (n);
+	return (bytes_written);
 }
