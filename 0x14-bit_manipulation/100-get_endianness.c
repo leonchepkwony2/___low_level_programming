04d4e5d0a9b1a8ac70c49dd1839b75250b8d8e36
leon <leon@Ubuntu.myguest.virtualbox.org>
2023-05-04 16:51:56 +0300

    redid bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness- function that checks the endianness
+ * Return: 0 (Big Endian) 1 (little endian)
+ */
+int get_endianness(void)
+{
+	int num = 1;
+
+	if (*(char *)&num == 1)
+		return (1);
+
+
+	return (0);
+}
